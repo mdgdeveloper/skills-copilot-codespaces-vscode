326d72ce33e8a1f0348d81b7bbaf29ec13c023e3
MDG developer <79763082+mdgdeveloper@users.noreply.github.com>
2023-12-03 16:14:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,70 +1,7 @@
-// Create a web server
-
+// Create web server
 const express = require('express');
 const router = express.Router();
-
 const Comment = require('../models/comment');
+const Post = require('../models/post');
+const User = require('../models/user');
 
-// POST /comments
-// Create a new comment
-router.post('/', function(req, res, next) {
-  const comment = new Comment({
-    name: req.body.name,
-    email: req.body.email,
-    message: req.body.message,
-    timestamp: Date.now()
-  });
-
-  comment.save(function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    res.status(201);
-    res.json(comment);
-  });
-});
-
-// GET /comments
-// Get a list of comments
-router.get('/', function(req, res, next) {
-  Comment.find({}, function(err, comments) {
-    if (err) {
-      return next(err);
-    }
-    res.json(comments);
-  });
-});
-
-// GET /comments/:id
-// Get a single comment
-router.get('/:id', function(req, res, next) {
-  Comment.findById(req.params.id, function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    if (!comment) {
-      return res.status(404).json({
-        message: 'Comment not found'
-      });
-    }
-    res.json(comment);
-  });
-});
-
-// DELETE /comments/:id
-// Delete a comment
-router.delete('/:id', function(req, res, next) {
-  Comment.findByIdAndRemove(req.params.id, function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    if (!comment) {
-      return res.status(404).json({
-        message: 'Comment not found'
-      });
-    }
-    res.json(comment);
-  });
-});
-
-module.exports = router;
