a4025930d42191668a65901351745f8539f92400
MDG developer <79763082+mdgdeveloper@users.noreply.github.com>
2023-12-03 16:06:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+}
